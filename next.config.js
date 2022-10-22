8eea1460a51931878a2e43415f1684c769757d44
ChristianT02 <christian.teheran02@gmail.com>
2022-10-21 20:14:35 -0500

    Settings to read csv

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.csv$/,
+      loader: "csv-loader",
+      options: {
+        dynamicTyping: true,
+        header: true,
+        skipEmptyLines: true,
+      },
+    });
+
+    return config;
+  },
+};
